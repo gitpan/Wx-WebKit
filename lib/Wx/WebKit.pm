5a6f16cde477a771d604729bde6fa73a9b109911
Dan Sugalski <dan@sidhe.org>
2006-05-08 17:36:44 +0000

    Import of DSUGAL/Wx-WebKit-0.02 from CPAN.
    gitpan-cpan-distribution: Wx-WebKit
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         DSUGAL/Wx-WebKit-0.02.tar.gz
    gitpan-cpan-author:       DSUGAL
    gitpan-cpan-maturity:     released

--- lib/Wx/WebKit.pm
+++ lib/Wx/WebKit.pm
@@ -5,32 +5,10 @@ use strict;
 use warnings;
 use Wx;
 
-require Exporter;
-use AutoLoader qw(AUTOLOAD);
+our $VERSION = '0.02';
 
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Wx::WebKit ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
-
-require XSLoader;
-XSLoader::load('Wx::WebKit', $VERSION);
+Wx::wx_boot('Wx::WebKit', $VERSION);
+package Wx::WebKitCtrl; our @ISA = 'Wx::Control';
 
 # Preloaded methods go here.
 
